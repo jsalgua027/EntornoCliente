fea27d41f37a3f6a1550ac3adacdd3a1c175cc37
jsalgua027 <jsalcedoguarde@gmail.com>
2023-11-21 12:49:49 +0100

    conversor

--- React/conversor/src/App.js
+++ React/conversor/src/App.js
@@ -1,25 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state={
+      euros:0,
+      factor:1.1,
+    }
+  }
+  aumentar(){
+    let auxeuro= this.state.euros+1;
+    this.setState({euros:auxeuro})
+
+  }
+  disminuir(){
+    let auxeuro= this.state.euros-1;
+    this.setState({euros:auxeuro})
+
+  }
+  render(){
+    return (
+      <div className="App">
+        {this.state.euros} Euros equivalen a  {this.state.euros*this.state.factor} dólares
+        <button onClick={()=>this.aumentar()}>+</button> 
+         <button  onClick={()=>this.disminuir()}>-</button>
+      </div>
+    
+    );
+  }
+  }
+  
 
 export default App;
+
