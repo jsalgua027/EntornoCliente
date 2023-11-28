1b96cbde9400ed3ec460bfd596741961fd6048a5
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-11-28 17:49:01 +0100

    iniciado mine

---
+++ React/minevreact/src/componentes/Botoncito.js
@@ -0,0 +1,12 @@
+
+  import Button from 'react-bootstrap/Button';
+
+  const Botoncito =()=>{
+    return(
+
+        <button variant="primary"></button>
+    )
+
+  }
+
+  export default Botoncito;
---
+++ React/minevreact/src/componentes/MatrizBotones.js
@@ -0,0 +1,15 @@
+import Botoncito from "./Botoncito";
+
+
+const MatrizBotones =()=>{
+  const botones= ['boton 1','boton 2', 'boton3'];
+  return(
+    <Botoncito ></Botoncito>
+
+
+  );
+
+
+}
+
+export default MatrizBotones
