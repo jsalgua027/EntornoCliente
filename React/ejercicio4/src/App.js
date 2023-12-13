d46cbdc75c1e5bc4cf7633fa5832326eb8d8567d
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-12-13 19:46:25 +0100

    terminado Ejercicio4React

--- React/ejercicio4/src/App.js
+++ React/ejercicio4/src/App.js
@@ -1,36 +1,49 @@
 import logo from "./logo.svg";
 import "./App.css";
-import React, { Component } from 'react';
-import UserList from './componentes/UserList';
+import React, { Component } from "react";
+import UserList from "./componentes/UserList";
 import User from "./componentes/User";
+import UserForm from "./componentes/UserForm";
 
 class App extends React.Component {
-  constructor(){
-
+  constructor() {
     super();
     this.state = {
-    users: [
-    {id: 1, name: "perico", email: "perico@myfpschool.com"},
-    {id: 2, name: "juanico", email: "juanico@myfpschool.com"},
-    {id: 3, name: "andrés", email: "andrés@myfpschool.com"}
-    ]
+      users: [
+        { id: 1, name: "perico", email: "perico@myfpschool.com" },
+        { id: 2, name: "juanico", email: "juanico@myfpschool.com" },
+        { id: 3, name: "andrés", email: "andrés@myfpschool.com" },
+      ],
     };
   }
-  render(){
+  handleOnAddUser(event) {
+    event.preventDefault();
+    let user = {
+      name: event.target.name.value,
+      email: event.target.email.value,
+    };
+    let tmp = this.state.users;
+    tmp.push(user);
+    this.setState({
+      users: tmp,
+    });
+  }
+  render() {
     return (
       <div className="App">
         <div className="App-header">
-                <h2>Me mola Myfpschool</h2>
+          <h2>Me mola Myfpschool</h2>
         </div>
         <div>
           <p>
             <strong>Añade usuarios</strong>
           </p>
+          <UserList users={this.state.users} />
+          <UserForm onAddUser={(e)=>this.handleOnAddUser(e)} />
         </div>
       </div>
     );
   }
- 
 }
 
 export default App;
--- React/ejercicio4/src/componentes/User.js
+++ React/ejercicio4/src/componentes/User.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 class User extends Component {
-render() {
-return(
-<li>
-{this.props.name} - {this.props.email}
-</li>
-);
-}
+  render() {
+    return (
+      <li>
+        {this.props.name} - {this.props.email}
+      </li>
+    );
+  }
 }
 export default User;
