61ab231a11bcdeefcbdd8f20403eef7bef158142
jsalgua027 <jsalcedoguarde@gmail.com>
2023-12-18 10:42:30 +0100

    terminado el calculador de cuotas

--- React/prestamo/src/App.js
+++ React/prestamo/src/App.js
@@ -12,23 +12,30 @@ class App extends React.Component {
     }
   handleOnAddUser(event){
     event.preventDefault();
-    const cantidad= event.target.cantidad.value;
-    const interes=event.target.interes.value;
-    const anios=event.target.anios.value;
-    const meses=event.target.meses.value;
-    const tiempoTotal= (anios*12)+meses;
-    console.log(cantidad);
-    console.log(interes);
-    console.log(anios);
-    console.log(meses);
-    console.log(tiempoTotal);
-    
-
-    
-      const resultado = cantidad*((Math.pow((1+interes),tiempoTotal)*interes)/((Math.pow((1+interes),tiempoTotal))-1));
-      console.log(resultado)
-      this.setState({cuota:resultado})
-    
+    const cantidad = parseFloat(event.target.cantidad.value);
+    const tasaAnual = parseFloat(event.target.interes.value) / 100;  // Convertir a decimal
+    const anios = parseInt(event.target.anios.value, 10);
+    const meses = parseInt(event.target.meses.value, 10);
+    const tiempoTotal = (anios * 12) + meses;
+  
+    console.log("Cantidad:", cantidad);
+    console.log("Tasa Anual:", tasaAnual);
+    console.log("Años:", anios);
+    console.log("Meses:", meses);
+    console.log("Tiempo Total:", tiempoTotal);
+  
+    const tasaPorPeriodo = tasaAnual / 12;  // Dividir por 12 para obtener la tasa por mes
+  
+    const numerador = Math.pow(1 + tasaPorPeriodo, tiempoTotal) * tasaPorPeriodo;
+    const denominador = Math.pow(1 + tasaPorPeriodo, tiempoTotal) - 1;
+  
+    console.log("Numerador:", numerador);
+    console.log("Denominador:", denominador);
+  
+    const resultado = cantidad * (numerador / denominador);
+    console.log("Cuota del préstamo:", resultado);
+  
+    this.setState({ cuota: resultado });
    
     
 
