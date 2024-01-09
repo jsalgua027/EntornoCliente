c2120ce54ebc253ca8327d528d637a1a9480de36
jsalgua027 <jsalcedoguarde@gmail.com>
2024-01-09 12:58:37 +0100

    mmm

--- React/ejercicio4hooks/src/App.js
+++ React/ejercicio4hooks/src/App.js
@@ -5,9 +5,14 @@ import User from './componentes/User';
 import UserForm from './componentes/UserForm';
 
 function App() {
-  const[nombre, setNombre]=useState();
+  const[nombre, setNombre]=useState([
+    { id: 1, nombre: "perico", correo: "perico@myfpschool.com" },
+    { id: 2, name: "juanico", email: "juanico@myfpschool.com" },
+    { id: 3, name: "andrés", email: "andrés@myfpschool.com" },
+  ]);
   const[correo, setCorreo]=useState();
   
+
   return (
     <div className="App">
       <h1>Ejercicio 4 con hooks</h1>
