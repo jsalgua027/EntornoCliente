5a7c17e55c3ed5d5c8b9b06b53b121b07b6af51c
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-01-17 18:20:46 +0100

    iniciado ejercicios de recuperacion

---
+++ React/EJERCICIOSRECUPERA/mi-primera-app/src/App.js
@@ -0,0 +1,53 @@
+import "./App.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "reactstrap";
+
+class App extends React.Component {
+  constructor(props) {
+    super();
+    this.state = {
+      saludo: "Esta aplicacion saluda en varios idiomas",
+    };
+  }
+  mensajeIngles(){
+    this.setState({saludo:"Hello"})
+  }
+  mensajeAleman(){
+    this.setState({saludo:"Hallo"})
+  }
+  mensajeSueco(){
+    this.setState({saludo:"Hej"})
+  }
+  mensajeEspanol(){
+    this.setState({saludo:"Hola"})
+  }
+
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Mi primera APP traductor</h1>
+        <br/>
+        <Button onClick={()=>this.mensajeIngles()} color="primary" outline >
+          Inglés
+        </Button> {' '}
+        <Button onClick={()=>this.mensajeAleman()} color="info" outline >
+          Alemán
+        </Button> {' '}
+        <Button onClick={()=>this.mensajeSueco()} color="warning" outline >
+          Sueco
+        </Button> {' '}
+        <Button onClick={()=>this.mensajeEspanol()} color="danger" outline >
+          Español
+        </Button>
+        <br/>
+        <br/>
+        <br/>
+        <h3>{this.state.saludo}</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
