cef4f1d30c3d9650aad473835a2508f11a8520f6
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-01-18 18:45:28 +0100

    ejercicio 3 y 4 con hooks

--- React/3listadeseoshooks/src/App.js
+++ React/3listadeseoshooks/src/App.js
@@ -1,25 +1,106 @@
 import logo from './logo.svg';
 import './App.css';
+import React,{useState,useEffect} from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
-  return (
-    <div className="App">
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'reactstrap';
+
+
+//Componente funcional UserList
+const UserList=(props)=>{
+ return(
+   <div>
+     {props.listaUsuarios.map((u)=>{
+       return(
+         <p key={u.id}>
+           <span>{u.name} - {u.email}</span>
+           <Button color='danger' 
+           onClick={()=>props.f(u.id)}>Borrar</Button>
+
+         </p>
+       )
+     })}
+   </div>
+ );
+}
+
+//Componente funcional formulario
+const UserForm=(props)=>{
+ const [name, setName] = useState("");
+ const [email, setEmail] = useState("");
+
+ const handleClick = () => {
+     props.f(name, email);
+ }
+
+ return (
+     <div>
+         <input placeholder='nombre' type='text' id='name' name='name' 
+         onChange={event => setName(event.target.value)} />
+         <input placeholder='email' type='text' id='email' name='email' 
+         onChange={event =>setEmail(event.target.value)}/>
+         <Button color='primary' onClick={handleClick}>Guardar</Button>
+            </div>
+ );
+}
+
+
+
+
+function App(){
+ const[usuarios,setUsuarios]=useState([
+   {id:1, name:"perico", email:"perico@gmail.com"},
+   {id:2, name:"juanico", email:"juanico@gmail.com"},
+   {id:3, name:"andres", email:"andres@gmail.com"},
+   
+ ]);
+
+
+
+
+ const handleOnAddUser=(name,email)=>{
+   let lista=[...usuarios];
+   // let id=lista.length > 0 ? Math.max(...l.map(o => o.id)) + 1 : 1;
+   let id=lista.length>0 ? (lista.length+1):1;
+
+   let obj={id:id, name:name, email:email}
+
+   lista.push(obj);
+   console.log("Insertado: " + id);
+   setUsuarios(lista);
+
+ }
+
+   const deleteUser=(id)=>{
+     let lista=[...usuarios];
+     lista=lista.filter(o => o.id !==id);
+     console.log("Borrado: "+ id);
+
+     setUsuarios(lista);
+   }
+
+
+   return (
+   <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+       <h1>Lista de usuarios-hooks</h1>
+        <img src={logo} className="App-logo" alt="logo"/>
+       {/* imagen del logo de react */}
+        <div>
+        <p>Añade tu usuario: </p>
+         <UserList listaUsuarios={usuarios} f={deleteUser}/>
+         <UserForm f={(x,y)=>handleOnAddUser(x,y)}/>
+        </div>
+         
+       </header>
+   </div>
+ );
+ 
+ 
 }
 
 export default App;
+
+
+
--- React/ejercicio4hooks/src/App.js
+++ React/ejercicio4hooks/src/App.js
@@ -1,23 +1,102 @@
 
-import  React ,{ useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "reactstrap";
+import React,{useState,useEffect} from "react";
 
-import User from './componentes/User';
-import UserForm from './componentes/UserForm';
+//Componente funcional ItenLista
+const ItemList=(props)=>{
+  return(
+    <ul>
+      {props.listaItems.map((d,index)=>(
+        <li key={index}>
+         {d} 
+        {/* <Borrar deseo={d} quitar={(e)=>props.quitar(e)}/> */}
+        <Button onClick={()=>props.quitar(d)} color="danger">X</Button>
+        </li>
+      )
+      )}
+    </ul>
+  );
 
-function App() {
-  const[users, setUsers]=useState([
-    { id: 1, nombre: "perico", correo: "perico@myfpschool.com" },
-    { id: 2, name: "juanico", email: "juanico@myfpschool.com" },
-    { id: 3, name: "andrés", email: "andrés@myfpschool.com" },
-  ]);
-  
+}
 
-  return (
-    <div className="App">
-      <h1>Ejercicio 4 con hooks</h1>
-      <UserForm/>
-    </div>
+//Componente funcional de boton Borrar
+const Borrar=(props)=>{
+ return(
+  <Button onClick={()=>props.quitar(props.deseo)} color="danger">X</Button>
+ );
+}
+
+//Componente funcional deseo
+const Input=(props)=>{
+  const[item,setItem]= useState("");
+  const handleInputChange=(event)=>{
+    setItem(event.target.value);
+  };
+  return(
+    <form onSubmit={(e)=>props.onAddItem(e,item)}>
+      <input 
+      type="text" placeholder="Escribe aquí tu deseo" name="deseo"
+      value={item} onChange={(e)=>handleInputChange(e)}
+      />
+      <button type="submit">Agregar</button>
+    </form>
   );
 }
+
+
+
+//Componente funcional App
+function App () {
+  const[listaItems,setListaItems]=useState(["GAMBAS","JAMÓN"]);
+  const[mensaje,setMensaje]=useState("");
+
+  function quitar(element){
+    let aux=listaItems.filter((i)=>i !==element);
+    setListaItems(aux);
+    console.log("Item borrado: "+ aux);
+  };
+
+  const handleAddItem=(event,nuevo)=>{
+    event.preventDefault();
+    const aux=[...listaItems];
+
+    let repetido=aux.filter((e)=>e===nuevo).length>0;
+
+    if(repetido){
+      console.log("item rep");
+      setMensaje("item repetido");
+    }
+    else if(nuevo.trim() ===""){
+      setMensaje("campo vacío");
+    }else{
+      aux.push(nuevo);
+      setListaItems(aux);
+      setMensaje("");
+    }
+  };
+
+    return (
+      <div className="App">
+        <header className="App-header">
+        <h1>Lista de listaItems-hooks</h1>
+         <img src={logo} className="App-logo" alt="logo"/>
+        {/* imagen del logo de react */}
+         <div>
+         <p>Añade tu regalo: </p>
+         <ItemList listaItems={listaItems}
+         quitar={(e)=>quitar(e)} />
+         <Input onAddItem={(e,deseo)=>handleAddItem(e,deseo)}/>
+         <p>{mensaje}</p>
+
+         </div>
+          
+        </header>
+      </div>
+    );
+  }
+
 
 export default App;
