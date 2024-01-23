c93ece24c56b799b1b9c70cfc31a60e9c5fd5162
jsalgua027 <jsalcedoguarde@gmail.com>
2024-01-23 10:33:46 +0100

    iniciado colocacion Damas

---
+++ React/EJERCICIOSCLASE/colocaciondamas/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Button } from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+function Botonera(props){
+//COMPONENTE QUE RENDERIZA EL TABLERO
+}
+class App extends Component {
+constructor(props) {//DAMAS. EXAMEN REACTJS.
+super(props);
+this.state = {
+//DEFINE EL ESTADO DE TU COMPONENTE PRINCIPAL
+//Recuerda que si defines una tabla 8x8 tu estado será inválido.
+
+};
+}
+componentWillMount() {
+//ESTE MÉTODO SE EJECUTARÁ AL PRINCIPIO DE LA APLICACIÓN. ANTES DE RENDERIZAR.
+}
+render() {
+return (
+<div className="App">
+<header className="App-header">
+
+</header>
+</div>
+);
+}
+}
+export default App;
