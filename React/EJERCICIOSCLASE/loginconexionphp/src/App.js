49298a3f75088b12b4ab3c73222093537664b05a
jsalgua027 <jsalcedoguarde@gmail.com>
2024-01-31 11:15:15 +0100

    funcion md5

--- React/EJERCICIOSCLASE/loginconexionphp/src/App.js
+++ React/EJERCICIOSCLASE/loginconexionphp/src/App.js
@@ -1,25 +1,57 @@
 import logo from './logo.svg';
 import './App.css';
+import {Button} from 'reactstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AppLogin from './componentes/AppLogin'
+import Menu from './componentes/Menu'
+import { Component } from 'react';
+import {PHPLOGIN} from './componentes/Datos';
+import axios from 'axios';
+import md5 from 'md5';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      menuItem:"UNO",
+      logged:false,
+    }
+  }
+  
+  changeMenu(item){
+    this.setState({menuItem:item})
+  }
+
+  userLogin(telefono,password){
+    /*
+    if (telefono=="Myfpschool" && password=="2023"){
+      this.setState({logged:true})
+    }
+    */
+    axios.post(PHPLOGIN,JSON.stringify({
+      telefono:telefono,
+      password:password
+    }))
+    .then(res => {
+      console.log(res.data.usuario);
+      if ( res.data.usuario !== undefined){
+        this.setState({logged:true});
+      }
+    })
+  }
+
+  render(){
+    let obj=<><Menu menuItem={this.state.menuItem} changeMenu={(item)=>this.changeMenu(item)} /></>
+    if (!this.state.logged){
+      obj=<AppLogin userLogin={(telefono,password)=>this.userLogin(telefono,password)}/>
+    }
+    return (
+      <div className="App">
+        {obj}
+      </div>
+    );
+  }
 }
 
 export default App;
+
---
+++ React/EJERCICIOSCLASE/loginconexionphp/src/componentes/Datos.js
@@ -0,0 +1,1 @@
+export const PHPLOGIN = "http://localhost/Proyectos/Curso23_24PHP/Curso23_24PHP/WebWorkesJava/ConexionReact/login/login.php";
