c3482110117bf27b09e4575a492d52f65136aff3
jsalgua027 <jsalcedoguarde@gmail.com>
2024-02-07 12:50:19 +0100

    fenotipo

--- React/EJERCICIOSCLASE/fototipo/src/App.js
+++ React/EJERCICIOSCLASE/fototipo/src/App.js
@@ -1,29 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Button, Col, Row } from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import preguntasRespuestas from "./datos/preguntasRespuestas.json";
+
+const ListaPreguntas = (props) => {
  
-const PreguntasCompleta=(props)=>{
-               
- return(
-  <>
-   {props.lista.map(e=>(<h2>{e.pregunta}</h2>))}
+
+  return props.lista.map((elemento) => {
+    return <div className="pregunta">
+      <Pregunta  arrayRespuesas={elemento.respuestas} textoPregunta={elemento.pregunta} arrayValores={elemento.valorRespuesta}/>
+    </div> 
+  });
+};
+
+const Pregunta = (props) => {
+  console.log(props.pre);
+  return (
+    <>
+      <h2>{props.textoPregunta}</h2>
+      {props.arrayRespuesas.map((elemento, indice) => {
+        return <Button color="primary" valor={props.arrayValores[indice]} outline>{elemento} </Button>;
+      })}
+    </>
+  );
+}
+
+const fotoTipoResultado=(props)=>{
 
 
-  </>
- ) 
-  }
+}
 
 
+function recojoDatos(event){
+console.log(event+"--------Entra")
 
+}
 
 function App() {
-  const listaPre=preguntasRespuestas.preguntas
+  const listaPre = preguntasRespuestas.preguntas;
 
   
+
   return (
     <div className="App">
-   
-<PreguntasCompleta lista={listaPre}/>
+      <h1>Descubre tu fototipo</h1>
+      <ListaPreguntas lista={listaPre} />
     </div>
   );
 }
