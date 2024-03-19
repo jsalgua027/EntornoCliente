6eb0abd80a4ef34ad1547e6af7183b7626692d5a
jsalgua027 <jsalcedoguarde@gmail.com>
2024-03-19 12:45:46 +0100

    inicio react

---
+++ RECUPERACION/React/ejercicio1botones/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Button } from 'reactstrap';
+import
+
+
+
+
+class app  extends Component {
+  constructor(props) {
+    super(props);
+  }
+  state = {  }
+  render() { 
+    return (   <>
+      <Button></Button>
+    
+    </>
+  )}
+}
+ 
+export default app;
---
+++ RECUPERACION/React/ejercicio1botones/src/componentes/Boton.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+    Button,
+    Card,
+    CardImg,
+    CardBody,
+    CardTitle,
+    CardText,
+  } from "reactstrap";
+
+
+  const Boton = (props) => {
+    return (
+      <>
+       <Button>hola Aqui estoy</Button>
+      </>
+    );
+  };
+  export default Boton;
