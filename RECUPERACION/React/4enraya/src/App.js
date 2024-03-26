319a079765b6af5be806943548a93d7f55ba20df
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-03-26 20:22:43 +0100

    al 60%

--- RECUPERACION/React/4enraya/src/App.js
+++ RECUPERACION/React/4enraya/src/App.js
@@ -1,69 +1,109 @@
-import logo from './logo.svg';
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from 'reactstrap';
-import React, { Component } from 'react'
-
-
-
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "reactstrap";
+import React, { Component } from "react";
 
 const Tablero = (props) => {
-  const estilo={
-    border: '1px solid black' // Borde negro sólido
-   }
+  const estilo = {
+    border: "1px solid black", // Borde negro sólido
+  };
+  console.log(props.tabla);
   const tablero = [];
   for (let i = 0; i < props.filas; i++) {
     const fila = [];
     for (let c = 0; c < props.columnas; c++) {
-      let color;
-      if (i === 0) {
-        color =color="secondary"; // Color gris para la fila 0
-        console.log("gris")
-      } else {
-        console.log("trasparente")
-        color = color = "transparent"; // Color del jugador para las otras filas
-      }
+   if(props.tabla[i][c])
+   {
+    fila.push(
+      <Button
+        outline
+        style={estilo}
+        onClick={() => props.pulsar(i, c)}
+      ></Button>
+    );
+   }else{
+    fila.push(
+      <Button
+        color={props.jugador}
+        estado={props.vacio}
+        style={estilo}
+        onClick={() => props.pulsar(i, c)}
+      ></Button>
+    );
+   }
 
-      fila.push( <Button color={props.color} style={estilo}></Button>);
+     
     }
     tablero.push(<div key={i}>{fila}</div>);
   }
-  return (
-    <div>
-      {tablero}
-    </div>
-  );
-}
+  return <div>{tablero}</div>;
+};
 
-
-
-
- class App extends Component {
+class App extends Component {
   constructor(props) {
     super(props);
-    this.state={
-      filas:7,
-      columnas:7,
-      vacio:true, //true vacio
-      jugador:["primary","warning"], //  jugador 1 azul, jugador 2 amarillo
-      ganador:true,
-      turno:1
-
-    }
+    this.state = {
+      listaBotones: JSON.parse(JSON.stringify(Array(9).fill(Array(9).fill("x")))),
+      filas: 9,
+      columnas: 9,
+      jugador: ["secondary", "primary", "warning"], //  jugador 1 azul, jugador 2 amarillo
+      ganador: true,
+      turno: 1,
+    };
   }
+
+ 
+
+  clicar(i, c) {
+    console.log(i, c);
+    let listaAux=this.state.listaBotones;
+    if (i == 0) {
+      if (this.state.turno == 1) {
+        // si es turno del jugador uno
+        let posicion=this.encontrarVacio(c)
+        console.log(posicion);
+        if(posicion>0){
+            listaAux[posicion][c]="R";
+
+        }
+        this.setState({ turno: 2 });
+        //uso el metodo de encontrarrrrrrr
+      } else {
+        // si es turno del jugador 2
+        this.setState({ turno: 1 });
+      }
+    } else {
+      console.log("nada");
+    }
+    this.setState({listaBotones:listaAux});
   
+  }
+  encontrarVacio(c) {
+    let lista = this.state.listaBotones;
+    for (let i = 0; i < 9; i++) {
+      if (lista[i][c] != "x") return i - 1;
+    }
+    return 8;
+  }
+
   render() {
     return (
-      <div className='juego'>
+      <div className="juego">
         <h1>4 EN RAYA</h1>
-       <Button color={this.state.jugador[1]}>Es el turno del jugador</Button>
-        <Tablero filas={this.state.filas} columnas={this.state.columnas}  vacio={this.state.vacio} jugador={this.state.jugador}/>
+        <Button color={this.state.jugador[1]}>
+          Es el turno del jugador {this.state.turno}
+        </Button>
+        <Tablero
+          tabla={this.state.listaBotones}
+          filas={this.state.filas}
+          columnas={this.state.columnas}
+          jugador={this.state.jugador}
+          pulsar={(i, c) => this.clicar(i, c)}
+        />
       </div>
-    )
+    );
   }
 }
-
-
-
 
 export default App;
