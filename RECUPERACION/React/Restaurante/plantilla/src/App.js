ea3d895ca0179eb22ffeabed1869c6b956736de6
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-04-17 21:04:33 +0200

    La conexión a la BD funciona; me traigo la lista de productos según categoría y muestro la info más las fotos

--- RECUPERACION/React/Restaurante/plantilla/src/App.js
+++ RECUPERACION/React/Restaurante/plantilla/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ProductosPorCategoria from './componentes/ProductosPorCategoria';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Productos por Categoría</h1>
+      <ProductosPorCategoria idCategoria={4} /> {/* Cambia el ID de la categoría según sea necesario */}
     </div>
   );
 }
 
 export default App;
+
---
+++ RECUPERACION/React/Restaurante/plantilla/src/componentes/ProductosPorCategoria.js
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function ProductosPorCategoria({ idCategoria }) {
+  const [productos, setProductos] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [nombreCategoria, setNombreCategoria] = useState("");
+  //solicitud
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get('http://localhost/Proyectos/Curso23_24PHP/Restaurante/productos.php?id_categoria=' + idCategoria);
+
+        console.log(response.data); 
+        setProductos(response.data);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error al obtener productos:', error);
+      }
+    };
+
+    fetchData();
+  }, [idCategoria]);
+
+  useEffect(() => {
+    
+// en vez de realizar una conexion como tengo la id de la categoria  usada , genero un array de objetos para sacar el nombre
+    const categorias = [
+      { id_categoria: 1, nombre: "Hamburguesas" },
+      { id_categoria: 2, nombre: "Bebidas" },
+      { id_categoria: 3, nombre: "Complementos" },
+      { id_categoria: 4, nombre: "Ensaladas" },
+      { id_categoria: 5, nombre: "Postres" }
+      
+    ];
+    // encuentro la categoria
+    const categoriaEncontrada = categorias.find(categoria => categoria.id_categoria === idCategoria);
+    if (categoriaEncontrada) {
+      setNombreCategoria(categoriaEncontrada.nombre);
+    }
+  }, [idCategoria]);
+
+
+
+  if (loading) {
+    return <div>Cargando productos...</div>;
+  }
+
+  return (
+    <div>
+     <h2>{nombreCategoria}</h2>
+      <ul>
+        {productos.map(producto => (
+          <li key={producto.id_producto}>{producto.producto}
+              <img src={`./images/${producto.id_categoria.toString()}/${producto.portadaFoto}`} alt={producto.producto}   style={{ width: '200px', height: 'auto' }}/>
+             {console.log(producto.id_categoria)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ProductosPorCategoria;
