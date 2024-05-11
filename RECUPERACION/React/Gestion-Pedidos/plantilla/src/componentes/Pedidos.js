1ec89b04e856df2f576a0044626da3ed0736af04
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-05-11 18:18:42 +0200

    Ya moficamos el estado del pedido

--- RECUPERACION/React/Gestion-Pedidos/plantilla/src/componentes/Pedidos.js
+++ RECUPERACION/React/Gestion-Pedidos/plantilla/src/componentes/Pedidos.js
@@ -1,7 +1,52 @@
 import React from 'react';
 import { Card, CardBody, CardTitle, CardText, Button } from 'reactstrap';
+import axios from 'axios';
 
-function Pedidos({ pedidos, onButtonAction }) {
+function Pedidos({ pedidos }) {
+/*
+  // Función para manejar la acción del botón
+  const handleButtonAction = (idPedido) => {
+     console.log("El dato que estoy enviando es:" +idPedido)
+    // Realizar la llamada a la API para actualizar el estado de entrega del pedido
+    axios.post( 'http://localhost/Proyectos/Curso23_24PHP/Restaurante/estadoPedidoAPI.php'//casa
+    // 'http://localhost/Proyectos/Curso23_24PHP/Curso23_24PHP/Restaurante/estadoPedidoAPI.php'//clase
+    ,{ id_pedido: idPedido } )
+   
+      .then(response => {
+        console.log("esoooooooooo:"+response.data);
+        // Aquí puedes actualizar el estado local si es necesario
+      })
+      .catch(error => {
+        console.error('Error al llamar a la API:', error);
+      });
+  };
+
+*/
+  const handleButtonAction = (idPedido) => {
+  //  console.log("El dato que estoy enviando es:" + idPedido);
+  
+    // Configuración de la solicitud POST
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ id_pedido: idPedido })
+    };
+    console.log("El dato que estoy enviando es:" +{ id_pedido: idPedido });
+    // Realizar la llamada a la API para actualizar el estado de entrega del pedido
+    fetch('http://localhost/Proyectos/Curso23_24PHP/Restaurante/estadoPedidoAPI.php', requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        console.log("esoooooooooo:", data);
+        // Aquí puedes actualizar el estado local si es necesario
+      })
+      .catch(error => {
+        console.error('Error al llamar a la API:', error);
+      });
+  };
+
+
   // Dividir los pedidos en grupos de tres
   const gruposDeTresPedidos = [];
   for (let i = 0; i < pedidos.length; i += 3) {
@@ -30,7 +75,7 @@ function Pedidos({ pedidos, onButtonAction }) {
                     ))}
                   </ul>
                 </CardText>
-                <Button color="primary" onClick={() => onButtonAction(pedido.id_pedido)}>Acción</Button>
+                <Button color="primary" onClick={() => handleButtonAction(pedido.id_pedido)}>Acción</Button>
               </CardBody>
             </Card>
           ))}
